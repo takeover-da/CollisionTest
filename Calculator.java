40b0d3103cf7ec18f79fb432aa9243430c382a32
takeover-da <dlslyn@naver.com>
2023-12-14 15:33:18 +0900

    ,.

--- Calculator.java
+++ Calculator.java
@@ -3,10 +3,12 @@ int my_add(int number1, int number2){
 	        return;
 	return number1+number2;
 //Computer1에서 작업
+
 int my_add(int number1, int number2){
  	if (number1 + number2 < 0)
                 return;
         return number1+number2;
 //Computer2 작업
+
     }
 }
