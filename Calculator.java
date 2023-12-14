7503a475de8ce3a648ed7e769043dadce3af739f
takeover-da <dlslyn@naver.com>
2023-12-14 15:26:38 +0900

    ...

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@ int my_add(int number1, int number2){
 	if (number1 < 0 or number2 < 0)	
 	        return;
 	return number1+number2;
-
+//Computer1에서 작업
 int my_add(int number1, int number2){
  	if (number1 + number2 < 0)
                 return;
